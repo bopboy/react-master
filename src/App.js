781afd7c4ca198c40e959c0539d78df5acbf4fb5
bopboy <39391891+bopboy@users.noreply.github.com>
2022-02-26 18:27:04 +0900

    09 스타일드 컴포넌트 + 가상 선택자 연습

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import styled, { keyframes } from 'styled-components'
 
 const Wrapper = styled.div`
   display: flex;
+  height: 100vh;
+  width: 100vw;
+  justify-content: center;
+  align-items: center;
 `
 const rotationAnimation = keyframes`
   0% {
@@ -16,6 +20,9 @@ const rotationAnimation = keyframes`
     border-radius:0px;
   }
 `
+const Emoji = styled.span`
+  font-size: 36px;
+`
 const Box = styled.div`
   height: 200px;
   width: 200px;
@@ -24,13 +31,9 @@ const Box = styled.div`
   justify-content: center;
   align-items: center;
   animation: ${rotationAnimation} 1s linear infinite;
-  span {
-    font-size: 36px;
+  ${Emoji} {
     &:hover {
-      font-size: 50px;
-    }
-    &:active {
-      opacity: 0;
+      font-size: 98px;
     }
   }
 `
@@ -38,8 +41,9 @@ function App() {
   return (
     <Wrapper>
       <Box>
-        <span>😗</span>
+        <Emoji as="span">😗</Emoji>
       </Box>
+      <Emoji as="span">😎</Emoji>
     </Wrapper>
   )
 }
