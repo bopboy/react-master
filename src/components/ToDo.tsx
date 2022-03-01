c2d03a3eb74f1aac6ef01ab312d560c341099529
bopboy <39391891+bopboy@users.noreply.github.com>
2022-03-01 13:40:17 +0900

    52 카테고리 변경 (불변성 유지)

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -6,6 +6,12 @@ function ToDo({ text, category, id }: IToDo) {
     const setToDos = useSetRecoilState(toDoState)
     const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
         const { currentTarget: { name } } = event
+        setToDos(oldToDos => {
+            const targetIndex = oldToDos.findIndex(toDo => toDo.id === id)
+            const newToDo = { text, id, category: name as any }
+            console.log("replace the to do in the index", targetIndex, "with", newToDo)
+            return [...oldToDos.slice(0, targetIndex), newToDo, ...oldToDos.slice(targetIndex + 1)]
+        })
     }
     return (
         <li>
