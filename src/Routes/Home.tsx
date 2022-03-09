0d4087a8992336773027064ef199b41fee269351
bopboy <39391891+bopboy@users.noreply.github.com>
2022-03-10 06:26:36 +0900

    99 슬라이더 2

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -37,26 +37,41 @@ const Slider = styled.div`
 `
 const Row = styled(motion.div)`
     display: grid;
-    gap: 10px;
+    gap: 5px;
     grid-template-columns: repeat(6, 1fr);
     position: absolute;
     width: 100%;
 `
-const Box = styled(motion.div)`
+const Box = styled(motion.div) <{ bgPhoto: string }>`
     background-color: white;
+    background-image: url(${props => props.bgPhoto});
+    background-size: cover;
+    background-position: center center;
     height: 200px;
     color: red;
-    font-size: 66px;
+    font-size: 36px;
 `
 const rowVariants = {
-    hidden: { x: window.outerWidth + 10 },
+    hidden: { x: window.outerWidth + 5 },
     visible: { x: 0 },
-    exit: { x: -window.outerWidth - 10 }
+    exit: { x: -window.outerWidth - 5 }
 }
+const offset = 6
 function Home() {
     const { data, isLoading } = useQuery<IGetMoviesResult>(["movies", "nowPlaying"], getMovies)
     const [index, setIndex] = useState(0)
-    const increaseIndex = () => setIndex(prev => prev + 1)
+    const [leaving, setLeaving] = useState(false)
+    const toggleLeaving = () => setLeaving(prev => !prev)
+    const increaseIndex = () => {
+        if (data) {
+            if (leaving) return
+            toggleLeaving()
+            const totalMovies = data.results.length - 1
+            const maxIndex = Math.floor(totalMovies / offset) - 1
+            setIndex(prev => prev === maxIndex ? 0 : prev + 1)
+
+        }
+    }
     return (
         <Wrapper>
             {isLoading ? (<Loader>Loading...</Loader>) : (<>
@@ -65,7 +80,7 @@ function Home() {
                     <Overview>{data?.results[0].overview}</Overview>
                 </Banner>
                 <Slider>
-                    <AnimatePresence>
+                    <AnimatePresence initial={false} onExitComplete={toggleLeaving}>
                         <Row
                             variants={rowVariants}
                             initial="hidden"
@@ -74,8 +89,11 @@ function Home() {
                             transition={{ type: "tween", duration: 1 }}
                             key={index}
                         >
-                            {[1, 2, 3, 4, 5, 6].map(i => (
-                                <Box key={i}>{i}</Box>
+                            {data?.results.slice(1).slice(offset * index, offset * index + offset).map(movie => (
+                                <Box
+                                    key={movie.id}
+                                    bgPhoto={makeImagePath(movie.backdrop_path, "w500")}
+                                />
                             ))}
                         </Row>
                     </AnimatePresence>
